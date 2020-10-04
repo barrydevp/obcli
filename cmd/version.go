00b3958f600e7fd8fb5f8b9fc83a522c6981cb55
barry <barrydevp@gmail.com>
2020-10-04 23:15:49 +0700

    add command

---
+++ cmd/version.go
@@ -0,0 +1,38 @@
+/*
+Copyright © 2020 barrydevp <barrydevp@gmail.com>
+Foobla CLI application foo.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Obcli version",
+	Long: `Print the version of Obcli
+
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("version called")
+		fmt.Println(args)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// versionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2020 barrydevp <barrydevp@gmail.com>
+Foobla CLI application foo.
+*/
+package main
+
+import "github.com/barrydevp/obcli/cmd"
+
+func main() {
+	cmd.Execute()
+}
